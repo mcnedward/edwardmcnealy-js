78a9f66fbaebb1ae4465b8b91888da9215bc3165
mcnedward <edwardmcnealy@gmail.com>
2017-03-11 14:58:01 -0500

    Add revision fingerprints and update caching

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var gulp = require('gulp'),
     cssmin = require('gulp-cssmin'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
+    rev = require('gulp-rev'),
+    del = require('del'),
+    path = require('path'),
     sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['watch']);
@@ -14,7 +17,7 @@ gulp.task('default', ['watch']);
 // Lints javascript and builds less on file changes
 gulp.task('watch', function() {
   gulp.watch('app/js/**/*.js', ['jshint', 'build-scripts']);
-  gulp.watch('app/less/**/*.less', ['build-less']);
+  gulp.watch('app/less/**/*.less', ['build-styles']);
 })
 
 // Lints javascript
@@ -25,7 +28,7 @@ gulp.task('jshint', function() {
 })
 
 // Builds all scripts to the public directory
-gulp.task('build-scripts', function(cb) {
+gulp.task('build-scripts', ['clean-script-rev'], function() {
   return gulp.src('app/js/**/*.js')
     .pipe(babel({
       presets: ['es2015']
@@ -34,25 +37,32 @@ gulp.task('build-scripts', function(cb) {
     .pipe(concat('app.min.js'))
     .pipe(uglify().on('error', uglifyError))
     // .pipe(sourcemaps.write('maps'))
-    .pipe(gulp.dest('public/js'));
+    .pipe(rev())
+    .pipe(gulp.dest('public/js'))
+    .pipe(rev.manifest({
+      base: '',
+      merge: true
+    }))
+    .pipe(gulp.dest(''));
 })
 
 // Builds all less to the public directory
-gulp.task('build-less', function() {
-  // Build the main style into one file
-  buildStyles('app/less/style.less', 'style.css');
-  // Build the rest
+gulp.task('build-styles', ['clean-styles-rev', 'build-styles-main', 'build-styles-portfolio']);
+gulp.task('build-styles-main', function() {
+  return buildStyles('app/less/style.less', 'style.css');
+})
+gulp.task('build-styles-portfolio', function() {
   return buildStyles([
     'app/less/blackjack.less',
     'app/less/colorzones.less',
     'app/less/modal.less',
     'app/less/parser.less',
-  ], 'portfolioStyles.css')
-});
+  ], 'portfolioStyle.css');
+})
 
 function buildStyles(input, output) {
   return gulp.src(input)
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(less().on('error', function(err) {
       gutil.log('Error in build-less task...');
       gutil.err(err);
@@ -62,8 +72,37 @@ function buildStyles(input, output) {
       gutil.err(err);
     }))
     .pipe(concat(output))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('public/css'));
+    // .pipe(sourcemaps.write())
+    .pipe(rev())
+    .pipe(gulp.dest('public/css'))
+    .pipe(rev.manifest({
+      base: '',
+      merge: true
+    }))
+    .pipe(gulp.dest(''));
+}
+
+// Clean up tasks
+gulp.task('clean-script-rev', function() {
+  return clean('app.min.js', path.join('public', 'js'));
+})
+gulp.task('clean-styles-rev', function() {
+  var dir = path.join('public', 'css');
+  clean('style.css', dir)
+  return clean('portfolioStyle.css', dir);
+})
+function clean(fileKey, dir) {
+  try {
+    var manifest = require('./rev-manifest.json');
+    var oldFilePath = manifest[fileKey];
+    if (!oldFilePath) {
+      return gutil.noop();
+    }
+    console.log('deleting ' + path.join(dir, oldFilePath))
+    return del(path.join(dir, oldFilePath));
+  } catch(e) {
+    return gutil.noop();
+  }
 }
 
 gulp.task('build-scripts-ugly', function() {
@@ -75,7 +114,7 @@ gulp.task('build-scripts-ugly', function() {
 })
 
 // Builds both scripts and less to the public directory
-gulp.task('build', ['build-scripts', 'build-less']);
+gulp.task('build', ['build-scripts', 'build-styles']);
 
 function uglifyError(err) {
   gutil.log(gutil.colors.red('[Error]'), err.toString());
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
+      compression = require('compression'),
       fileUpload = require('express-fileupload'),
       path = require('path'),
       routes = require('./routes/routes'),
+      manifest = require('./rev-manifest.json')
       port = 8484;
 
 global.environment = 'production';
 
 const modules = path.join(__dirname, 'node_modules'),
       lib = path.join(__dirname, 'public', 'js', 'lib'),
-      oneDay = 86400000,
-      options = {
+      oneDay = 31536000,
+      cacheOptions = {
         maxAge: oneDay,
         etag: true
       };
@@ -24,32 +26,35 @@ if (global.environment === 'dev') {
   cssExt = '.min.css';
 }
 // Modules and libraries
-app.use('/js/tether', express.static(path.join(modules, 'tether', 'dist', 'js', 'tether' + jsExt), options));
-app.use('/js/bootstrap', express.static(path.join(modules, 'bootstrap', 'dist', 'js', 'bootstrap' + jsExt), options));
-app.use('/js/jquery', express.static(path.join(modules, 'jquery', 'dist', 'jquery' + jsExt), options));
-app.use('/js/angular', express.static(path.join(modules, 'angular', 'angular' + jsExt), options));
-app.use('/js/angular-ui-router', express.static(path.join(modules, 'angular-ui-router', 'release', 'angular-ui-router' + jsExt), options));
-app.use('/js/angular-animate', express.static(path.join(modules, 'angular-animate', 'angular-animate' + jsExt), options));
-app.use('/js/ui-bootstrap', express.static(path.join(modules, 'angular-ui-bootstrap', 'dist', 'ui-bootstrap-tpls' + jsExt), options));
-app.use('/js/angular-recaptcha', express.static(path.join(modules, 'angular-recaptcha', 'release', 'angular-recaptcha' + jsExt), options));
-app.use('/js/knockout', express.static(path.join(modules, 'knockout', 'build', 'output', 'knockout-latest' + jsExt), options));
-app.use('/js/moment', express.static(path.join(modules, 'moment', 'min', 'moment' + jsExt), options));
-app.use('/js/moment-timezone', express.static(path.join(modules, 'moment-timezone', 'builds', 'moment-timezone-with-data' + jsExt), options));
-app.use('/js/classie', express.static(path.join(lib, 'classie' + jsExt), options));
-app.use('/js/cssParser', express.static(path.join(lib, 'cssParser' + jsExt), options));
-app.use('/js/modernizr', express.static(path.join(lib, 'modernizr' + jsExt), options));
-app.use('/css/tether', express.static(path.join(modules, 'tether', 'dist', 'css', 'tether' + cssExt), options)); 
-app.use('/css/bootstrap', express.static(path.join(modules, 'bootstrap', 'dist', 'css', 'bootstrap' + cssExt), options));
-app.use('/css', express.static(path.join(modules, 'bootstrap', 'dist', 'css'), options));
+app.use('/js/tether', express.static(path.join(modules, 'tether', 'dist', 'js', 'tether' + jsExt), cacheOptions));
+app.use('/js/bootstrap', express.static(path.join(modules, 'bootstrap', 'dist', 'js', 'bootstrap' + jsExt), cacheOptions));
+app.use('/js/jquery', express.static(path.join(modules, 'jquery', 'dist', 'jquery' + jsExt), cacheOptions));
+app.use('/js/angular', express.static(path.join(modules, 'angular', 'angular' + jsExt), cacheOptions));
+app.use('/js/angular-ui-router', express.static(path.join(modules, 'angular-ui-router', 'release', 'angular-ui-router' + jsExt), cacheOptions));
+app.use('/js/angular-animate', express.static(path.join(modules, 'angular-animate', 'angular-animate' + jsExt), cacheOptions));
+app.use('/js/ui-bootstrap', express.static(path.join(modules, 'angular-ui-bootstrap', 'dist', 'ui-bootstrap-tpls' + jsExt), cacheOptions));
+app.use('/js/angular-recaptcha', express.static(path.join(modules, 'angular-recaptcha', 'release', 'angular-recaptcha' + jsExt), cacheOptions));
+app.use('/js/knockout', express.static(path.join(modules, 'knockout', 'build', 'output', 'knockout-latest' + jsExt), cacheOptions));
+app.use('/js/moment', express.static(path.join(modules, 'moment', 'min', 'moment' + jsExt), cacheOptions));
+app.use('/js/moment-timezone', express.static(path.join(modules, 'moment-timezone', 'builds', 'moment-timezone-with-data' + jsExt), cacheOptions));
+app.use('/js/classie', express.static(path.join(lib, 'classie' + jsExt), cacheOptions));
+app.use('/js/cssParser', express.static(path.join(lib, 'cssParser' + jsExt), cacheOptions));
+app.use('/js/modernizr', express.static(path.join(lib, 'modernizr' + jsExt), cacheOptions));
+app.use('/css/tether', express.static(path.join(modules, 'tether', 'dist', 'css', 'tether' + cssExt), cacheOptions)); 
+app.use('/css/bootstrap', express.static(path.join(modules, 'bootstrap', 'dist', 'css', 'bootstrap' + cssExt), cacheOptions));
+app.use('/css/font-awesome', express.static(path.join(__dirname, 'public', 'css', 'font-awesome.min.css'), cacheOptions));
 // My stuff
-app.use('/js/app', express.static(path.join(__dirname, 'public', 'js', 'app.min.js')));
-app.use('/css', express.static(path.join(__dirname, 'public', 'css'), options));
-app.use('/css', express.static(path.join(__dirname, 'public', 'css'), options));
-app.use('/fonts', express.static(path.join(__dirname, 'public', 'fonts'), options));
-app.use('/img', express.static(path.join(__dirname, 'public', 'img'), options));
+app.use('/js/app', express.static(path.join(__dirname, 'public', 'js', manifest['app.min.js'])));
+app.use('/css/style', express.static(path.join(__dirname, 'public', 'css', manifest['style.css']), cacheOptions));
+app.use('/css/portfolioStyle', express.static(path.join(__dirname, 'public', 'css', manifest['portfolioStyle.css']), cacheOptions));
+app.use('/fonts', express.static(path.join(__dirname, 'public', 'fonts'), cacheOptions));
+app.use('/img', express.static(path.join(__dirname, 'public', 'img'), cacheOptions));
 app.use(express.static(path.join(__dirname, 'public', 'views')));
 
 app.use(fileUpload());
+app.use(compression({
+  level: 1
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type:'application/json'}));
 app.use(bodyParser.urlencoded({extended: true})); 
